54098bc96f589394279994d29e55cf56f2630c99
annerland <annaacampelo@gmail.com>
2025-10-06 20:25:05 +0200

    fix: removing aggressive minification settings (unsafe can break vue's reactivity system)

--- vite.config.ts
+++ vite.config.ts
@@ -12,19 +12,11 @@ export default defineConfig({
         drop_console: true,
         drop_debugger: true,
         pure_funcs: ['console.log', 'console.info', 'console.debug'],
-        passes: 2,
-        unsafe: true,
-        unsafe_comps: true,
-        unsafe_math: true,
-        unsafe_proto: true,
-        unsafe_regexp: true,
-        unsafe_undefined: true,
+        passes: 1,
       },
       mangle: {
-        toplevel: true,
-        properties: {
-          regex: /^_/,
-        },
+        toplevel: false,
+        properties: false,
       },
       format: {
         comments: false,
